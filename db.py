9303d6ade27a8504d26b0fa265b04ea5beb78134
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2025-08-19 08:10:24 -0300

    algunas cosas

--- db.py
+++ db.py
@@ -1,9 +1,20 @@
 import mysql.connector
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",        
-        user="root",             
-        password="root",
-        database="copa_renault"
-    )
+    print("Connecting to the database...")
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",        
+            user="root",             
+            password="root",
+            database="aula_virtual",
+            port=3306,
+        )
+        print("Connection established.")
+        return conn
+    except mysql.connector.Error as err:
+        print(f"❌ Error: {err}")
+
+if __name__ == "__main__":
+    connection = get_db_connection()
+    print("Connection object:", connection)
